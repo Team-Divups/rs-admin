4ae7b4fc5925102f69deea6b86ff522002c38b1a
Hana Jiffry <hanajiffryscg@gmail.com>
2022-06-01 21:55:10 +0530

    user list connected with backend and data loaded

--- src/Components/Modules/Admin/ViewClientList.js
+++ src/Components/Modules/Admin/ViewClientList.js
@@ -1,91 +1,74 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { FaAddressBook, FaCheck, FaEdit } from 'react-icons/fa';
+import {FaRegAddressCard, FaRegEdit, FaTrashAlt } from 'react-icons/fa';
 import{
     Card,
     CardBody,
     CardSubtitle,
     CardTitle,
-    Table,   
+    Table,  
 } from "reactstrap";
-import user1 from '../../../Assets/user1.jpg';
-import user2 from '../../../Assets/user2.jpg';
-import user3 from '../../../Assets/user3.jpg';
-import user4 from '../../../Assets/user4.jpg';
-import user5 from '../../../Assets/user5.jpg';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const tableData = [
-    {
-      avatar: user1,
-      name: "Hanna Gover",
-      role: "Admin",
-      email: "hgover@gmail.com",
-    },
-    {
-      avatar: user2,
-      name: "Hanna Gover",
-      role: "Marketing Team",
-      email: "hgover@gmail.com",
-    },
-    {
-      avatar: user3,
-      name: "Hanna Gover",
-      role: "User",
-      email: "hgover@gmail.com",
-    },
-    {
-      avatar: user4,
-      name: "Hanna Gover",
-      role: "Main Admin",
-      email: "hgover@gmail.com",
-    },
-    {
-      avatar: user5,
-      name: "Hanna Gover",
-      role: "Sales Team",
-      email: "hgover@gmail.com",
-    },
-  ];
+import user2 from "../../../Assets/user2.jpg";
 
 const ViewClientList= ()=> {
+
+  const [userData,setUserData]= useState([]);
+
+   // Using useEffect to call the API once mounted and set the data
+   useEffect(() => {(async (e) => {
+      e.preventDefault();
+      const result = await axios("http://localhost:3001/adminuser");
+      setUserData(result.data);})
+      ();
+  }, []);
+
     return(
         <>
         <Card>
             <CardBody>
                 <CardTitle tag="h5">All Admin Users</CardTitle>
                 <CardSubtitle className="mb-2 text-muted" tag="h6">Overview of Users</CardSubtitle>
-
+               
                 <Table className="no-wrap mt-3 align-middle" responsive >
                    <thead>
                        <tr>
-                           <th>User Details</th>
-                           <th>User Name</th>
-                           <th>User Role</th>
+                           <th>User</th>
+                           <th>Designation</th>
+                           <th>Role</th>
+                           <th>Joined on</th>
                            <th>Actions</th>
                        </tr>
                    </thead>
                    <tbody>
-                     { tableData.map((tdata,index)=>(
-                         <tr key={index} className="border-top">
-                           <td>
-                               <div className="d-flex align-items-center p-2">
-                                   <img
-                                    src={tdata.avatar}
-                                    className="rounded-circle"
-                                    alt="avatar"
-                                    width="45"
-                                    height="45"
-                                   />
-                                   <div className="ms-2">
-                                     <span className="text-muted">{tdata.email}</span>
-                                   </div>
-                               </div> 
-                           </td>
-                           <td>{tdata.name}</td>
+                     { userData.map((tdata,index)=>(
+                        <tr key={index} className="border-top">
+                          <td>
+                            <div className="d-flex align-items-center p-2">
+                              <img
+                              src={user2}
+                              className="rounded-circle"
+                              alt="avatar"
+                              width="45"
+                              height="45"/>
+                              <div className="ms-3">
+                                <h6 className="mb-0">{tdata.name}</h6>
+                                <span className="text-muted">{tdata.email}</span>
+                              </div>
+                            </div>
+                          </td>
+                 
+                           <td>{tdata.designation}</td>
                            <td>{tdata.role}</td>
+                           <td>{tdata.joined_at}</td>
+                           
                            <td>
-                              <FaEdit paddingRight="10px"/>
-                              <FaAddressBook paddingLeft="20px"/>
-                              <FaCheck paddingLeft="20px"/>
+                              <tr>
+                                <td><FaRegEdit/></td>
+                                <td><FaRegAddressCard/></td>
+                                <td><FaTrashAlt/></td>
+                              </tr>
                            </td>
                          </tr>
                      ))
