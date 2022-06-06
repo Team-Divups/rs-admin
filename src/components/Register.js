a38ea4429e620952cbd113229f9dff3ef98dc634
Anushan Santhirakumar <73377435+Anushan1508@users.noreply.github.com>
2022-06-06 12:47:35 +0530

    user Auth Finished

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,16 +1,12 @@
-import React from 'react'
 import { useRef, useState, useEffect } from "react";
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import './Register.css'
-import axios from '../../api/axios';
-// import { Link } from "react-router-dom";
-const REGISTER_URL = '/register';
-
-
+import axios from '../api/axios';
+import { Link } from "react-router-dom";
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = '/register';
 
 const Register = () => {
     const userRef = useRef();
@@ -175,7 +171,7 @@ const Register = () => {
                     <p>
                         Already registered?<br />
                         <span className="line">
-                            {/* <Link to="/">Sign In</Link> */}
+                            <Link to="/">Sign In</Link>
                         </span>
                     </p>
                 </section>
